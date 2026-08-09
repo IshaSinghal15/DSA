1aaea444171d9a68b35ce4d552d7915475a94fe5
IshaSinghal15 <153821772+IshaSinghal15@users.noreply.github.com>
2026-08-09 11:25:49 +0530

    Time: 1 ms (76.54%) | Memory: 10.8 MB (83.46%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string dir;
+
+        while (getline(ss, dir, '/')) {
+            if (dir.empty() || dir == ".") {
+                continue;
+            }
+
+            if (dir == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            }
+            else {
+                st.push_back(dir);
+            }
+        }
+
+        string ans = "/";
+
+        for (int i = 0; i < st.size(); i++) {
+            ans += st[i];
+
+            if (i != st.size() - 1)
+                ans += "/";
+        }
+
+        return ans;
+    }
+};
