b9c63fa471388a1b8999a7e2a51d1f35d93f0244
IshaSinghal15 <153821772+IshaSinghal15@users.noreply.github.com>
2026-08-09 11:24:43 +0530

    Time: 4 ms (45.52%) | Memory: 10 MB (43.26%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string next = "";
+
+            for (int j = 0; j < s.size(); ) {
+                int count = 0;
+                char ch = s[j];
+
+                while (j < s.size() && s[j] == ch) {
+                    count++;
+                    j++;
+                }
+
+                next += to_string(count);
+                next += ch;
+            }
+
+            s = next;
+        }
+
+        return s;
+    }
+};
