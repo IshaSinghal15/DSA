6f6b4043e1c25cc55ce266a52364d41d9ccb4c9d
IshaSinghal15 <153821772+IshaSinghal15@users.noreply.github.com>
2026-08-12 23:40:24 +0530

    Time: 3 ms (70.29%) | Memory: 32 MB (35.15%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,28 @@
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+
+    void pushLeft(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+
+        pushLeft(node->right);
+
+        return node->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
