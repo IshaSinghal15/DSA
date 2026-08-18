bfbe9afaf6b67d11abad3bb268f7731d7c98f20a
IshaSinghal15 <153821772+IshaSinghal15@users.noreply.github.com>
2026-08-18 21:01:25 +0530

    Time: 5 ms (18.12%) | Memory: 21.8 MB (72.62%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+        int mid = (n - 1) / 2;
+        int high = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) nums[i] = sorted[mid--];
+            else nums[i] = sorted[high--];
+        }
+    }
+};
